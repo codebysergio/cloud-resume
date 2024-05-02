16daaa3a9181c5e46818fecd99f2ad3692c55096
sergio <sergiogutierrez6619@gmail.com>
2024-05-02 03:12:16 -0700

    dbscript.py rework

--- terraform/dbscript.py
+++ terraform/dbscript.py
@@ -1,26 +1,9 @@
-import json 
 import boto3
-dynamodb = boto3.resource ('dynamodb')
-table = dynamodb.table('web-views')
-def lambda_handler (event, context):
-    response = table.get_item(
-        Key={
-            'id': '1'
-        }
-    )
-    views = response ['Item' ]['views']
-    views = views + 1
-    print(views)
-    response = table.put_item(Item={
-    'id': '1',
-    'views': views
-    })
+dynamodb = boto3.resource ('dynamodb',region_name='us-east-1')
+table = dynamodb.Table('Web-Views')
+response = table.get_item(
+    Key = {
+        'id': '1'
+})
 
-    return {
-        'statusCode': 200,
-        'headers':{
-            'content-type': 'application/json'
-        },
-        'body': views
-    }
-    
+print(response['Item'])
